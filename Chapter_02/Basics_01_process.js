6c57c33578f2e4c8936454b94e73fae4758e9151
Amit9881 <amitsuryawanshi041@gmail.com>
2026-05-07 19:49:47 +0530

    commit on 7th may 2026

--- Chapter_02/Basics_01_process.js
+++ Chapter_02/Basics_01_process.js
@@ -1,1 +1,2 @@
 console.log(process.platform);
+console.log("let me try");
---
+++ Chapter_02/Basics_07052026.js
@@ -0,0 +1,4 @@
+let name = "Surya";
+let cityName = "Karad";
+
+console.log("My name is " + name + " and I am from " + cityName);
